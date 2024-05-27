b333a6777c962b05765396030fd5756d49169ed5
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-05-27 17:16:37 -0300

    style: slint

--- folder-watch/src/main.rs
+++ folder-watch/src/main.rs
@@ -1,13 +1,19 @@
-pub mod git;
-use notify::{event::{CreateKind, ModifyKind, RemoveKind, RenameMode}, Event, RecursiveMode, Watcher};
-use std::{path::Path, sync::mpsc::channel, time::Duration};
+mod git;
 use chrono::prelude::*;
+use notify::{
+    event::{CreateKind, ModifyKind, RemoveKind, RenameMode},
+    Event, RecursiveMode, Watcher,
+};
+use std::{path::Path, sync::mpsc::channel, time::Duration};
 
-fn get_file_or_folder_name (path: &str) -> &str {
-    path.split('/').filter(|s| !s.is_empty()).last().unwrap_or("")
+fn get_file_or_folder_name(path: &str) -> &str {
+    path.split('/')
+        .filter(|s| !s.is_empty())
+        .last()
+        .unwrap_or("")
 }
 
-fn create_commit_date () -> String {
+fn create_commit_date() -> String {
     let local: DateTime<Local> = Local::now();
     let day = local.day();
     let month = local.month();
@@ -19,61 +25,76 @@ fn create_commit_date () -> String {
     return format!("[{}/{}/{} {}:{}] - ", month, day, year, hour, minutes);
 }
 
-fn commit_modify_event (event_kind: ModifyKind, event: Event) {
+fn commit_modify_event(event_kind: ModifyKind, event: Event) {
     let commit_prefix = create_commit_date();
     let current_path = event.paths[0].to_str().unwrap();
-    
+
     if let ModifyKind::Data(_) = event_kind {
         let commit_message = format!("edit {}", get_file_or_folder_name(current_path));
         git::commit_and_push(
             format!("{}{}", commit_prefix, commit_message).as_str(),
-            current_path
+            current_path,
         );
     }
     if ModifyKind::Name(RenameMode::Both) == event_kind {
         let new_path = event.paths[1].to_str().unwrap();
-        let commit_message = format!("rename {} to {}", get_file_or_folder_name(current_path), get_file_or_folder_name(new_path));
+        let commit_message = format!(
+            "rename {} to {}",
+            get_file_or_folder_name(current_path),
+            get_file_or_folder_name(new_path)
+        );
         git::commit_and_push(
             format!("{}{}", commit_prefix, commit_message).as_str(),
-            new_path
+            new_path,
         );
     }
-
 }
 
-fn commit_create_event (event_kind: CreateKind, file_source: &str) {
+fn commit_create_event(event_kind: CreateKind, file_source: &str) {
     let commit_prefix = create_commit_date();
     if event_kind == CreateKind::File {
-        let commit_message = format!("create new file called {}", get_file_or_folder_name(file_source));
-        git::commit_and_push(format!("{}{}", commit_prefix, commit_message).as_str(), file_source);
+        let commit_message = format!(
+            "create new file called {}",
+            get_file_or_folder_name(file_source)
+        );
+        git::commit_and_push(
+            format!("{}{}", commit_prefix, commit_message).as_str(),
+            file_source,
+        );
     }
 }
 
-fn commit_remove_event (event_kind: RemoveKind, file_source: &str) {
+fn commit_remove_event(event_kind: RemoveKind, file_source: &str) {
     let commit_prefix = create_commit_date();
     let file_or_folder_name = get_file_or_folder_name(file_source);
     if event_kind == RemoveKind::File {
         let commit_message = format!("delete {} file", file_or_folder_name);
-        git::commit_and_push(format!("{}{}", commit_prefix, commit_message).as_str(), file_source);
+        git::commit_and_push(
+            format!("{}{}", commit_prefix, commit_message).as_str(),
+            file_source,
+        );
     }
 }
 
 fn main() {
     let (tx, rx) = channel();
-    
+
     let mut watcher = notify::recommended_watcher(move |res| {
         let _ = tx.send(res);
-    }).unwrap();
+    })
+    .unwrap();
 
     let _ = watcher.watch(Path::new(".."), RecursiveMode::Recursive);
-    
+
     loop {
         match rx.recv_timeout(Duration::from_secs(30)) {
             Ok(event) => {
                 let event = event.unwrap();
                 let path = event.paths[0].to_str().unwrap();
                 // ignore files/folder change on .git folder
-                if path.contains("/.git") {continue;}
+                if path.contains("/.git") {
+                    continue;
+                }
                 if let notify::EventKind::Create(ev) = event.kind {
                     commit_create_event(ev, path);
                 }
@@ -84,7 +105,7 @@ fn main() {
                     commit_remove_event(ev, path)
                 }
             }
-            Err(_) => {} 
+            Err(_) => {}
         }
     }
 }
